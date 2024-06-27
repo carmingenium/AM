04f75ca9e02cd56629369fd27666ed2b90c49e0f
Dağhan Sever <daghansever@gmail.com>
2024-06-27 22:21:27 +0300

    focusing on getting the secret from github
    removing unnecessary starting code
    also setting up workflow

--- AM.py
+++ AM.py
@@ -1,21 +1,16 @@
 import os
 from discord import Intents, Client, Message
 from dotenv import load_dotenv
-# from responses import get_response
+from discord.ext import commands
+import discord
 
 #load_dotenv()
 #TOKEN = os.getenv('DISCORD_TOKEN')
 
-TOKEN = os.environ['DISCORD_TOKEN']
+TOKEN = os.environ["DISCORD_TOKEN"]
 
-# need to set intents
-intents: Intents = Intents.all()
-intents.message_content = True
-client: Client = Client(intents=intents)
-# client = Client()
+
 
 @client.event
 async def on_ready():
-  print(f'{client.user} has connected to Discord!')
-
-client.run(TOKEN)
+  print('Hello, I am ready!')
