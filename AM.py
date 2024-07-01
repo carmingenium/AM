3f6d832e7c6764600630f4ec60beb1a144ed04d6
Dağhan Sever <daghansever@gmail.com>
2024-07-01 18:15:26 +0300

    starting point of coding the bot

--- AM.py
+++ AM.py
@@ -3,11 +3,9 @@ import discord
 from discord import Intents, Client, Message
 from discord.ext import commands
 
-#load_dotenv()
-#TOKEN = os.getenv('DISCORD_TOKEN')
+
 
 TOKEN = os.environ["DISCORD_TOKEN"]
-TEST = os.environ["TEST"]
 BOT = commands.Bot(command_prefix='!', intents=Intents.all())
 
 @BOT.event
@@ -16,12 +14,8 @@ async def on_ready():
 
 def main():
   print("I RAN")
-  print(TEST)
-  if(TOKEN == None):
-    print("The token is empty")
-  else:
-    print("This is the token (secret): " + TOKEN)
+  print("This is the token (secret): " + TOKEN)
   
 
-  # BOT.run(TOKEN)
+  BOT.run(TOKEN)
 main()
