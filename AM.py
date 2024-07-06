0dfe34373c5b39db73a22728878b7d7c9e5ac8f2
Dağhan Sever <daghansever@gmail.com>
2024-07-06 14:02:53 +0300

    moving potential certification error fix to main code

--- AM.py
+++ AM.py
@@ -2,15 +2,15 @@ import os
 import discord
 from discord import Intents, Client, Message
 from discord.ext import commands
+import ssl # needed for certification ?? (testing)
 
-# bot does connect but does not run on_ready command and gets stuck
 
+# setup
 description = '''Description test'''
-
 TOKEN = os.environ["DISCORD_TOKEN"]
 intents1 = Intents.all()
-
 BOT = commands.Bot(command_prefix='!',description=description, intents=intents1)
+# setup end
 
 @BOT.event
 async def on_ready():
@@ -20,6 +20,7 @@ def main():
   print("I RAN")
   print("This is the token (secret): " + TOKEN)
   
+  ssl._create_default_https_context = ssl._create_unverified_context # needed for certification ?? (testing)
 
   BOT.run(TOKEN) # this is where the bot gets stuck
   # Bot does not run on_ready command when it runs
