a8436542cf2f7702d0b18ce93ad1e2b30fc0ca5f
Dağhan Sever <daghansever@gmail.com>
2024-07-08 17:52:33 +0300

    on_ready and on_disconnect events

--- AM.py
+++ AM.py
@@ -11,14 +11,34 @@ intents1 = Intents.all()
 BOT = commands.Bot(command_prefix='!',description=description, intents=intents1)
 # setup end
 
-@BOT.event
+@bot.event
 async def on_ready():
-  print('Hello, I am ready!')
+  print("bot is active")
+  print(f'{bot.user} has connected to Discord!')
+  dev_channel = bot.get_channel(DEV_CHANNEL)
+  await dev_channel.send("BOT ACTIVE")
+  
+@bot.event
+async def on_disconnect():
+  print("bot is disconnected")
+  dev_channel = bot.get_channel(DEV_CHANNEL)
+  await dev_channel.send("BOT DISCONNECTED")
+
+@bot.command(name='test')
+async def test(ctx):
+  await bot.close()
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
-  print("I RAN")
-  print("This is the token (secret): " + TOKEN)
-  
-
-  BOT.run(TOKEN)
+  bot.run(BOT_TOKEN)
 main()
