aef9b5bd5c4224a009c0424845189c30dff6b355
Dağhan Sever <daghansever@gmail.com>
2024-07-22 17:30:12 +0300

    interaction was used improperly it seems, testing

--- AM.py
+++ AM.py
@@ -12,23 +12,25 @@ intents1 = discord.Intents.all()
 intents1.members = True
 bot = commands.Bot(command_prefix='!', intents=intents1)
 # setup end
+dev_channel = bot.get_channel(DEV_CHANNEL)
+
 
 @bot.event
-async def on_ready(interaction: discord.Interaction): # quick note, on_ready is called whenever resume fails
+async def on_ready(): # quick note, on_ready is called whenever resume fails
   print("bot is active")
   print(f'{bot.user} has connected to Discord!')
-  dev_channel = bot.get_channel(DEV_CHANNEL)
+  # dev_channel = bot.get_channel(DEV_CHANNEL)
   await dev_channel.send("BOT ACTIVE")
   synced = await bot.tree.sync()
   print("synced" + str(len(synced)))
   # expire functionality
-  await expire(interaction) # on ready gets stuck on here
+  # await expire() # on ready gets stuck on here
   await dev_channel.send("Synced " + str(len(synced)) + " commands")
 
-async def expire(interaction): # this doesnt work currently.
-  await interaction.response.send_message("Bot will shut down in " + 5 + " seconds")
+async def expire(): # this doesnt work currently.
+  await dev_channel.send("Bot will shut down in " + 5 + " seconds")
   await asyncio.sleep(5)  # 6 hours 25 seconds normally. Will wait 6 hours 40 seconds to be sure
-  await interaction.response.send_message("Shutting down: " + str(bot.user) + " Reason: Expiration")
+  await dev_channel.send("Shutting down: " + str(bot.user) + " Reason: Expiration")
   await close_bot()
 
 @bot.tree.command(name="help",description="Shows all usable commands")
